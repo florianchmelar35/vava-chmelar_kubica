a37f8afce8362acc2c35cf4f9780b27ae4743cb8
florianchmelar35 <Trolo1998@gmail.com>
2018-05-06 19:21:27 +0200

    dzizs krajst

---
+++ src/MVC/controller/AddGroupController.java
@@ -0,0 +1,15 @@
+package MVC.controller;
+
+import MVC.Main;
+
+public class AddGroupController {
+    private Main main;
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
---
+++ src/MVC/controller/ListGroupController.java
@@ -0,0 +1,15 @@
+package MVC.controller;
+
+import MVC.Main;
+
+public class ListGroupController {
+    private Main main;
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
